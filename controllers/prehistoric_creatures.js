6eeaaf7ca1bd42d82de81efcfb2978ed0888c370
weston-bailey <wdbailey87@gmail.com>
2023-03-31 12:32:59 -0700

    solves lab

--- controllers/prehistoric_creatures.js
+++ controllers/prehistoric_creatures.js
@@ -42,4 +42,41 @@ router.get('/:id', (req, res) => {
     })
 })
 
+// GET /prehistoric_creatures/edit/:id -- GET the edit form
+router.get('/edit/:id', (req, res) => {
+	const creatures = readCreatures()
+    res.render('creatures/edit', {
+        creature: creatures[req.params.id],
+        id: req.params.id
+    })
+})
+
+// PUT /prehistoric_creatures/:id -- PUT/PATCH new info from edit form to creature
+router.put('/:id', (req,res) => {
+    const creatures = readCreatures()
+
+    // change name and type property of chosen creature to values from edit form
+    creatures[req.params.id].type = req.body.type
+    creatures[req.params.id].img_url = req.body.img_url
+
+    // save edited creature
+    fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatures))
+    
+    // redirect back to creature index
+    res.redirect('/prehistoric_creatures')
+})
+
+// DELETE /prehistoric_creatures/:id -- DELETE a single creature @ :id
+router.delete('/:id', (req, res) => {
+    const creatures = readCreatures()
+
+    // remove deleted creature from creature.json
+    creatures.splice(req.params.id, 1)
+
+    // save new creature file
+    fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatures))
+
+    // redirect to index route AKA GET /prehistoric_creatures
+    res.redirect('/prehistoric_creatures')
+})
 module.exports = router
