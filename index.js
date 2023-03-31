bea1e70598f1221ea80dc1f411e0555e7ad14f6f
weston-bailey <wdbailey87@gmail.com>
2023-03-31 14:57:02 -0700

    adds dino colors

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: false }))
 
 // middleware
 app.use(methodOverride('_method'))
+app.use(express.static("public"))
 
 // routes
 // GET / -- index show route for the app
